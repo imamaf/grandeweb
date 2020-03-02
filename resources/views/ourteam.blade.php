9c02078df96217df8b01c97c4d6d17330e3105a7
imamaf <imamaf647@gmail.com>
2020-03-02 23:15:43 +0700

    nambah effect parallax (cuil)

--- resources/views/ourteam.blade.php
+++ resources/views/ourteam.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>index</title>
+    <title>ourteam</title>
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/animate.css">
@@ -37,13 +37,13 @@
         </div>
     </nav>
 
-    <div class="jumbotron-our">
-        <div class="container">
-            <div class="title-our wow fadeInRightBig animated" data-wow-duration="3s">
-                <h1>We Make Your Dream</h1>
+        <div class="jumbotron-our">
+            <div class="container">
+                <div class="title-our wow fadeInRightBig animated" data-wow-duration="3s">
+                    <h1>We Make Your Dream</h1>
+                </div>
             </div>
         </div>
-    </div>
 
     <section>
         <div class="container" style="max-width:900px;">
@@ -115,9 +115,10 @@
             </div>
         
     </div> -->
+    
+
 </body>
 </html>
-
 <script src="js/jquery-3.1.0.js"></script>
 <script src="js/script.js"></script>
 <script type="text/javascript" src="js/owl.carousel.min.js"></script>
